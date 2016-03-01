df857e341dfc2198a8dc98260e339e52c0ece476
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-02-29 22:14:25 -0600

    Added circular Intergral term.

--- PIDController.java
+++ PIDController.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class PIDController {
     private double Kp, Ki, Kd;
     private double err, prevErr, intErr;
+    private double[] accumErr;
+    private int errIdx;
     private double setpoint;
     
     public PIDController() {
@@ -17,14 +19,28 @@ public class PIDController {
         err = 0;
         intErr = 0;
         prevErr = 0;
+        accumErr = new double[10];
+        for(double currErr : accumErr) currErr = 0;
+        errIdx = 0;
     }
     
     public double output(double input, double time) {
         err = setpoint - input;
     	System.out.println("ERROR: " + err);
         intErr += err*time;
-        double control = Kp*err + Ki*intErr + Kd*(err - prevErr) / time;
-		prevErr = err;
+
+        accumErr[errIdx] = err;
+        errIdx = (errIdx + 1) % accumErr.length;
+        
+        double windowIntErr = 0;
+        for(int i = 0; i < accumErr.length; i++) {
+    		windowIntErr += accumErr[i];
+        }
+        
+        
+        double control = Kp*err + Ki*windowIntErr + Kd*(err - prevErr) / time;
+		
+        prevErr = err;
         return control;
     }
     
